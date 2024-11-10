8e5f26f194138603a5bd3a1c85c0d15f54b962d4
Nurdaulet <baglanovbaglan227@gmail.com>
2024-11-11 00:00:37 +0500

    updated fully history and changed cards for main

---
+++ src/scripts/historyData.js
@@ -0,0 +1,33 @@
+// historyData.js
+const historyData = [
+    {
+        title: 'Founding of the Khanate',
+        period: '15th century',
+        fileName: 'founding_of_the_khanate',  // имя файла без расширения
+        image: 'path/to/map-image1.jpg'
+    },
+    {
+        title: 'Golden Age',
+        period: '16th - early 17th century',
+        fileName: 'golden_age',  // имя файла без расширения
+        image: 'path/to/map-image2.jpg'
+    },
+    {
+        title: 'Period of Crisis and Fragmentation',
+        period: '17th century',
+        fileName: 'crisis_and_fragmentation',  // имя файла без расширения
+        image: 'path/to/map-image3.jpg'
+    },
+    {
+        title: 'Struggle Against Jungar Invasions',
+        period: '18th century',
+        fileName: 'jungar_invasions',  // имя файла без расширения
+        image: 'path/to/map-image4.jpg'
+    },
+    {
+        title: 'Annexation by the Russian Empire',
+        period: '19th century',
+        fileName: 'annexation_by_russia',  // имя файла без расширения
+        image: 'path/to/map-image5.jpg'
+    }
+];
